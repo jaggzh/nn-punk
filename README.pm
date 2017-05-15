0349a21205503870426315641b306451a62d4c0e
Jaggzh <jaggz.h@gmail.com>
2017-05-15 02:10:56 -0700

    added snapshot of str colorization, and a README.pm

---
+++ README.pm
@@ -0,0 +1,12 @@
+nn-punk is a machine-learning project for text segmentation;
+the idea being to process un-punctuated text, breaking it into
+sentences (eg. speech to text).
+
+The project currently doesn't function well (ie. at all), but feel
+free to download it and work on it. :)
+
+Included is ansi.py, for terminal colors; it includes support,
+documented in ansi.py, for colorizing text. I use it for visualizing
+the weights.
+  ![STR Colorize Demo Snapshot](snapshots/ansi-py-str-colorize-for-nn-visualization.png)
+
